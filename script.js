0ea51c976dc1c60e68921555dc75ad37d170e8b1
shay5555 <62674745+shay5555@users.noreply.github.com>
2023-01-05 16:59:28 +0200

    Add visual confirm password verification

--- script.js
+++ script.js
@@ -3,16 +3,21 @@ const form = document.querySelector('form');
 const email = document.getElementById('email');
 const emailError = document.querySelector('.email-field  span.error');
 
+const password = document.getElementById('password');
+const passwordError = document.querySelector('.password-field span.error');
+const confirmPassword = document.getElementById('confirm-password');
+const confirmPasswordError = document.querySelector('.confirm-password-field span.error');
 
-email.addEventListener('focusin', () => {
+var isPasswordValidated = false;
+var isEmailValidated = false;
 
-});
 
 email.addEventListener('focusout', () => {
+
     if (email.validity.valid === false) {
         email.classList.remove('valid');
         email.classList.add('error');
-        emailError.textContent = "\tPlease enter a valid Email address.";
+        emailError.textContent = "Please enter a valid Email address.";
     } else {
         email.classList.remove('error');
         email.classList.add('valid');
@@ -20,9 +25,66 @@ email.addEventListener('focusout', () => {
     }
 });
 
-form.addEventListener('submit', (e) => {
-    if(!firstName.validity.valid) {
-        showError(firstName, firstNameError);
-        e.preventDefault();
+password.addEventListener('input', () => {
+    isPasswordValidated = false;
+    if (password.value.length === 0) {
+        password.classList.remove('valid');
+        password.classList.add('error');
+        passwordError.textContent = "Password field is empty";
+    
+    } else if (password.value !== confirmPassword.value && confirmPassword.value.length > 0) {
+        confirmPassword.classList.remove('valid');
+        confirmPassword.classList.add('error');
+        confirmPasswordError.textContent = "The passwords do not match.";
+    
+    } else if (password.value !== confirmPassword.value && confirmPassword.value.length === 0) {
+        confirmPassword.classList.remove('error');
+        confirmPassword.classList.add('valid');
+        confirmPasswordError.textContent = '';
+    }
+    
+    else {
+        password.classList.remove('error');
+        confirmPassword.classList.remove('error');
+        password.classList.add('valid');
+        confirmPassword.classList.add('valid');
+        confirmPasswordError.textContent = '';
+        isPasswordValidated = true;
+    }
+    
+    /* if (password.value === confirmPassword.value) {
+        confirmPassword.classList.remove('error');
+        confirmPassword.classList.add('valid');
+        confirmPassword.validity.valid = true;
+        confirmPasswordError.textContent = '';
+
+    } else if (password.value !== confirmPassword.value && confirmPassword.value.length > 0) {
+        confirmPassword.classList.remove('valid');
+        confirmPassword.classList.add('error');
+        confirmPassword.validity.valid = false;
+        console.log(confirmPassword.validity.valid);
+        confirmPasswordError.textContent = "The passwords do not match.";
+    } */
+});
+
+confirmPassword.addEventListener('input', () => {
+
+    if (password.value.length === 0 && confirmPassword.value.length > 0) {
+        confirmPassword.classList.remove('valid');
+        confirmPassword.classList.add('error');
+        isPasswordValidated = false;
+        confirmPasswordError.textContent = "Password field is empty.";
+
+    } else if (password.value.length > 0 && confirmPassword.value.length > 0 && password.value !== confirmPassword.value) {
+        confirmPassword.classList.remove('valid');
+        confirmPassword.classList.add('error');
+        isPasswordValidated = false;
+        confirmPasswordError.textContent = "The passwords do not match.";
+
+    } else if (password.value.length > 0 && password.value === confirmPassword.value) {
+        confirmPassword.classList.remove('error');
+        confirmPassword.classList.add('valid');
+        isPasswordValidated = true;
+        confirmPasswordError.textContent = '';
     }
 });
